a872342c16ee7b40a4d03c5607a8f752a0a0f20e
Davi Ferreira <daviferreiraaew@gmail.com>
2022-10-28 15:12:40 -0300

    Starting the logic for the counter

--- script.js
+++ script.js
@@ -4,32 +4,53 @@ const buttonSet = document.querySelector('.set')
 const buttonStop = document.querySelector('.stop')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 
-buttonPlay.addEventListener('click', function(){
+function countDown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+
+    if (seconds <= 0) {
+      seconds = 60
+    }
+    secondsDisplay.textContent = seconds - 1
+    countDown()
+  }, 1000)
+}
+
+buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+  countDown()
 })
 
-buttonPause.addEventListener('click', function() {
+buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
 })
 
-buttonStop.addEventListener('click', function() {
+buttonStop.addEventListener('click', function () {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 })
 
-buttonSoundOn.addEventListener('click', function(){
+buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
 })
 
-buttonSoundOff.addEventListener('click', function(){
+buttonSoundOff.addEventListener('click', function () {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
+})
+
+buttonSet.addEventListener('click', function () {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
